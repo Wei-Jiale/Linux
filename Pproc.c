673a87cc723f9585cb2df715010940a9c0726a9e
Wei-Jiale <576401521@qq.com>
2020-03-01 20:52:36 +0800

    The progress bar

---
+++ Pproc.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    int i = 0;
+    char bar[101] = {'\0'};
+    const char *lable = "|-\\";
+    for( ;i<=100;i++ )
+    {
+        printf("[%-100s][%d\%][%c]\r",bar,i,lable[i%4]);
+        fflush(stdout);
+        bar[i] = '#';
+        usleep(100000);     
+    }
+    printf("\n");
+    return 0;
+}
